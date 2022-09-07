c5ded2733c9363642f6e20608b225437a2b49652
BrianLu0101 <112454837+BrianLu0101@users.noreply.github.com>
2022-09-06 22:36:56 -0400

    Ch 1 - Exercise: Working with Data

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,29 @@
+print(10 * 5) 
+
+print(10 ** 2) 
+
+print(15 // 10) 
+
+print(-15 // 10 ) 
+
+print(15 % 10) 
+
+print(10 % 15)
+
+print(10 % 10) 
+
+print(0 % 10)
+
+# Prints the quotient of 10 divided by 15 in which gives you an infinite decimal but is not displayed to be infinite.
+print(10/15)
+
+
+rate = float(input("What is the current exchange rate from Euro to USD? "))
+
+amount = float(input("Enter the amount of Euro you wish to exchange: "))
+
+total = amount * rate
+
+result = total - 3.00
+
+print(result)
