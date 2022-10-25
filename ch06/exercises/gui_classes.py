0e57d2fc3e5f0f12ad2cf064a780e0b90a970109
BrianLu0101 <112454837+BrianLu0101@users.noreply.github.com>
2022-10-24 20:14:01 -0400

    Ch 6 - Exercise: GUI Classes

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,17 @@
+class Progress:
+    def __init__(self):
+        self.coins = 22
+        self.level = '1-1'
+        self.time = 367
+
+class Pipe:
+    def __init__(self):
+        self.useable = False
+        self.pirahna = False
+        self.color = 'green'
+
+class Goomba:
+    def __inir__(self):
+        self.num = 2
+        self.move = True
+        self.defeat = True
